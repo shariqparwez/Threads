fd7d38c549063e885cf744931be695d14d00c9bf
Shariq Parwez <sparwe@sapient.com>
2016-02-02 17:01:45 +0530

    Thread

---
+++ src/com/training/ThreadApplication3.java
@@ -0,0 +1,14 @@
+package com.training;
+
+public class ThreadApplication3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new UsePrintString("Pizza","Coke");
+		new UsePrintString("Idli","Sambhar");
+		new UsePrintString("Poori","Bhaji");
+		new UsePrintString("Parantha","Curd");
+		
+	}
+
+}
